81146b7ea672254787aa36941d13ad187744a8c0
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-09-12 10:38:40 +0300

    120921

--- route/user.js
+++ route/user.js
@@ -14,13 +14,16 @@ router.post('/login', (req, res, next) => {
                 user: user
             })
         }
+        //LLogin
         req.login(user, { session: false }, (err) => {
             if (err) {
                 console.log('user', user)
                 res.send(err)
             }
+            //User data to plain object data
+            const userTokenData = { username: user.dataValues.username, email: user.dataValues.email }
             // generate a signed son web token with the contents of user object and return it in the response
-            const token = jwt.sign(user, require('../config/token_config').secret_token_key)
+            const token = jwt.sign(userTokenData, require('../config/token_config').secret_token_key)
             return res.json({ user, token })
         })
     })(req, res, next)
@@ -32,10 +35,12 @@ router.get('/failureLogin', (req, res) => {
 
 //Signup
 router.post('/signup', (req, res) => {
-    console.log('denemeUser');
+    //Get user data
     const { username, email, password } = req.body
+    //Find user in table if exist return "exists message" if not exists create a new user
     User.findOne({ where: { username: username } }).then(user => {
         if (!user) {
+            //Hash user password
             const hashPassword = CryptoJS.AES.encrypt(password, username).toString();
             const newUser = {
                 username, email, password: hashPassword
