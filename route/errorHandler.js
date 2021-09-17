dd8095b44d2590883324cc7cd98c30e38dccb91a
BakanYusufSina <BakanYusufSina@users.noreply.github.com>
2021-09-17 17:29:47 +0300

    170921

--- route/errorHandler.js
+++ route/errorHandler.js
@@ -2,15 +2,9 @@ const passport = require('passport')
 
 module.exports = (req, res, next) => {
     passport.authenticate('jwt', { session: false }, (err, user, info) => {
-        console.log('user', user);
         if (err) return next(err)
         if (!user) {
-            return res.status(401).send({
-                "error": {
-                    "code": "INVALID_AUTHORIZATION_CODE",
-                    "message": "Invalid authorization code"
-                }
-            })
+            return res.status(401).json({ errorCode: 401 })
         }
         req.user = user
         next()
